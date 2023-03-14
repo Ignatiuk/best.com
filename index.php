ba9251a1f824340e9d2df7e43707c3108996c450
Alexander Ignatyuk <54143543+Ignatiuk@users.noreply.github.com>
2023-03-14 14:01:05 +0300

    Source code

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Викуля – лучшая</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml">
+</head>
+<body>
+    <div class="wrapper">
+        Этот сайт посвящен Вике Зеленяк – лучшей подруге на свете и одной из самых светлых людей в моей жизни.
+        <br><br>
+        С Днём рождения!
+        <br>
+        <span class="love">Люблю! ❤</span>
+    </div>
+    
+    <div class="bottom">
+        От твоего друга – Саши Игнатюка
+    </div>
+</body>
+</html>
